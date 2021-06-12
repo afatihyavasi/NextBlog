51928bff290979730d5cca26f9a0dd02c5565a45
afatihyavasi <afatihyavasi@gmail.com>
2021-06-12 22:18:46 +0300

    without end

--- config/database.js
+++ config/database.js
@@ -4,10 +4,10 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI'),
+        uri: env('DATABASE_URI','mongodb+srv://admin:deneme123@cluster0.6havq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
+        database: env('DATABASE_NAME','myFirstDatabase'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
